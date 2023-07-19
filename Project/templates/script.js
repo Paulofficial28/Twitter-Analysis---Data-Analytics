25782a0cf8b7f0f2815b1872bc20caf7c082f884
Paul Pandiyan K <116186148+Paulofficial28@users.noreply.github.com>
2023-07-19 20:46:57 +0530

    final deliverables

---
+++ Project/templates/script.js
@@ -0,0 +1,12 @@
+function navigateToDashboard() {
+    window.location.href = "dashboard.html";
+  }
+  
+  function navigateToStory() {
+    window.location.href = "story.html";
+  }
+  
+  function navigateToReport() {
+    window.location.href = "report.html";
+  }
+  
